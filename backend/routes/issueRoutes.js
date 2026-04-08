25ba84cf8638715daeadf1404c5a7db732a9d7cb
JEEVITH <jeevieth.official@gmail.com>
2026-04-08 22:22:09 +0530

    Delete backend directory

--- backend/routes/issueRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const issueController = require("../controllers/issueController");
-const router = express.Router();
-
-router.get("/", issueController.listIssuances);
-router.post("/", issueController.createIssuance);
-
-module.exports = router;
